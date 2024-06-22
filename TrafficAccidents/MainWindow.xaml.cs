64b3c8a687d2f176c2352fac0095d3443caac341
Marek von Rogall <Marek von Rogall@DESKTOP-VHTJ1MO>
2024-06-22 15:16:02 +0200

    UI changes. Added labels to display all information of a dataset entry

--- TrafficAccidents/MainWindow.xaml.cs
+++ TrafficAccidents/MainWindow.xaml.cs
@@ -26,11 +26,6 @@ namespace TrafficAccidents
             accidentMap.CoreWebView2.Navigate($"https://www.openstreetmap.org/?mlat={mlat}&mlon={mlon}");
         }
 
-        private void buttonDisplayEntry_Click(object sender, RoutedEventArgs e)
-        {
-            canvasDisplayData.Visibility = Visibility.Visible;
-        }
-
         private void boxAccidentID_showDS_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
             labelAccidentID_showDS.Visibility = Visibility.Hidden;
@@ -40,14 +35,54 @@ namespace TrafficAccidents
                 read.GetEntryById(cassandraDb.Session, id);
                 if (read.Accidents[0] != null)
                 {
-                    string[] coordinates = read.Accidents[0].GeoPoint.Split(',');
-                    string mlat = coordinates[0].Trim();
-                    string mlon = coordinates[1].Trim();
-                    ShowCoordinatesOnMap(mlat, mlon);
+                    DisplayAccident();
                 }
                 else { throw new Exception(); }
             }
             catch { labelAccidentID_showDS.Visibility = Visibility.Visible; }
+        }
+
+        private void DisplayAccident()
+        {
+            string[] coordinates = read.Accidents[0].GeoPoint.Split(',');
+            string mlat = coordinates[0].Trim();
+            string mlon = coordinates[1].Trim();
+            ShowCoordinatesOnMap(mlat, mlon);
+
+            labelType.Content = read.Accidents[0].Typ;
+            labelSeriousness.Content = read.Accidents[0].Schwere;
+
+            string[] numberDayOfWeek = read.Accidents[0].Wochentag.Split(' ');
+            string number = Convert.ToInt32(numberDayOfWeek[0].Trim()).ToString("D2");
+            string dayOfWeek = numberDayOfWeek[1].Trim();
+            string year = read.Accidents[0].Jahr.ToString();
+            string month = read.Accidents[0].Monat.ToString("D2");
+            string hour = read.Accidents[0].Stunde.ToString();
+
+            string date = $"{dayOfWeek}, der {number}.{month}.{year}";
+            labelDate.Content = date;
+
+            if(hour != "") { labelHour.Content = hour; }
+            else { labelHour.Content = "/"; }
+        }
+
+        private void buttonAddEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonModifyEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonDeleteEntry_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void buttonDisplayEntry_Click(object sender, RoutedEventArgs e)
+        {
+            canvasDisplayData.Visibility = Visibility.Visible;
         }
     }
 }
